8dcf8b569c05715e434ba96d1ab6826c75d33449
Devin Berkani <devinberkani@gmail.com>
2022-04-17 10:07:38 -0400

    update modal styling

--- beauty-salon-app-react/src/components/SingleClientModal.js
+++ beauty-salon-app-react/src/components/SingleClientModal.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { useGlobalContext } from './context';
+import data from '../data';
 
 const SingleClientModal = () => {
   const { showModal, closeModal } = useGlobalContext();
   return (
     <div className={`modal-overlay ${showModal && 'show-modal'}`}>
       <div className='modal-container'>
-        <h3>modal content</h3>
+        <h2>Client Info</h2>
         <button className='btn' onClick={closeModal}>
           Close
         </button>
